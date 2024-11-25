8bf3cade46d0ad8c5c2492c21a777322c2249078
kelvinhflau <kelvin.l1994@gmail.com>
2024-11-25 22:58:45 +0000

    remove prefix env var

--- src/app.py
+++ src/app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Hello World! - 25/11/2024 update - if this appears on render. This is good'
+   return 'Hello World! - 25/11/2024 update - Updated text 1'
